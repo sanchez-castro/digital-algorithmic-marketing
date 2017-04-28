ca4022810d2f61397a0e68c7be93328f30b00b25
schzcas <schzcas@gmail.com>
2017-04-28 13:23:57 -0500

    uploading first attempt

---
+++ homework/homework4-group/HW4.R
@@ -0,0 +1,19 @@
+# Digital and Algorithmic Marketing
+# Instructor: Sanjog Misra
+# Homework #4 (Group)
+
+# The persado_experiment.xlsx file contains a real experiment run by Persado
+# Use the data to answer the following questions.
+
+# Q1: Assuming that all relevant variables were tested how many possible
+#		message combinations are there? 
+# Q2: Estimate two logit models based on the data (assuming a simple linear 
+#		specification) - one for opens and another for clicks. Discuss your results based
+#		on the nature of the variables (see variable names and descriptions tab).
+# Q3: Use the estimated models to compute predicted probabilities for all possible message
+# 		combinations (separate predictions for opens and clicks). 
+#		Which messages have the highest fitted response probabilities (for opens and clicks each)?
+#		Are the two messages similar or different? Discuss.
+
+
+ 
